6cff9c2ab8ecbfcfcb99b73bf2404169f1077ee5
ID25 <xid25x@gmail.com>
2015-12-09 09:18:58 +0200

    Check if schema present for buttons

--- app/controllers/ryakuzu/application_controller.rb
+++ app/controllers/ryakuzu/application_controller.rb
@@ -1,11 +1,16 @@
 module Ryakuzu
   class ApplicationController < ActionController::Base
     rescue_from Errno::ENOENT, with: :render_500
+    helper_method :schema_present?
 
     private
 
     def render_500
       render template: 'ryakuzu/main/error_500', layout: 'ryakuzu/layouts/application', status: 500
+    end
+
+    def schema_present?
+      File.file?('db/schema.rb')
     end
   end
 end
