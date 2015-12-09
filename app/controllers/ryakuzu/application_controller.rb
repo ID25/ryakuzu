a3523929fd1f4ff3bb79c5537655f611480d0885
ID25 <xid25x@gmail.com>
2015-12-09 06:19:34 +0200

    Add exception, if schema.rb not found

--- app/controllers/ryakuzu/application_controller.rb
+++ app/controllers/ryakuzu/application_controller.rb
@@ -1,4 +1,11 @@
 module Ryakuzu
   class ApplicationController < ActionController::Base
+    rescue_from Errno::ENOENT, with: :render_500
+
+    private
+
+    def render_500
+      render template: 'ryakuzu/main/error_500', layout: 'ryakuzu/layouts/application', status: 500
+    end
   end
 end
