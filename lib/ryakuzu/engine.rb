1e891fdd569d303e777ea215e4936c3d5ce1c8c0
ID25 <xid25x@gmail.com>
2015-12-24 19:06:26 +0300

    Mount route before loading

--- lib/ryakuzu/engine.rb
+++ lib/ryakuzu/engine.rb
@@ -4,5 +4,11 @@ module Ryakuzu
   class Engine < ::Rails::Engine
     isolate_namespace Ryakuzu
     config.autoload_paths += %W(#{config.root}/lib)
+
+    initializer 'Add routes', before: :load_config_initializers do |app|
+      Rails.application.routes.append do
+        mount Ryakuzu::Engine, at: '/ryakuzu'
+      end
+    end
   end
 end
